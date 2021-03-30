da88894dafeaa5478791dac0775ddd363e4e98c4
Nyacist <old.deus.73@gmail.com>
2021-03-30 11:55:18 +0300

    dropdown
    dropdown with counter (pug, scss, js)

--- src/js/index.js
+++ src/js/index.js
@@ -4,30 +4,68 @@
 // import './styles/style.css'
 // import * as $ from 'jquery'
 // import './styles/scss.scss'
-//
-// function createAnalytics() {
-//     let counter = 0
-//     let destroyed = false
-//
-//     const listener = () => counter++
-//
-//     document.addEventListener('click', listener)
-//
-//     return {
-//         destroy() {
-//           document.removeEventListener('click', listener)
-//             destroyed = true
-//         },
-//         getClicks() {
-//             if (destroyed) {
-//                 return 'Analytics is destroyed'
-//             }
-//             return counter
-//         }
-//     }
-// }
-//
-// window.analytics = createAnalytics()
 
 import * as $ from 'jquery'
 import '../styles/style.scss'
+
+function resultStr(counter) {
+    let div = counter.querySelectorAll('.dropdown__count>h3');
+    let str = div[0].textContent +' ';
+    switch (div[0].textContent){
+        case '0':
+        case '5': {
+            str+= 'спален';
+            break;
+        }
+        case '1': {
+            str += 'спальня';
+            break;
+        }
+        default: str+= 'спальни';
+    }
+    str+= ', '+div[1].textContent+' ';
+    switch (div[1].textContent){
+        case '0':
+        case '5':
+            str+= 'кроватей';
+            break;
+        case '1':
+            str+= 'кровать';
+            break;
+        default: str+= 'кровати';
+    }
+    str+= '...';
+    counter.previousElementSibling.value = str;
+}
+
+for (let counter of document.querySelectorAll('.dropdown__counter')) {
+    counter.addEventListener('click', function (e) {
+        let sign = e.target.getAttribute('data-action');
+        if (sign == 'minus') {
+            let number = $(e.target).next().text();
+            if (number > 0) {
+                number--;
+                $(e.target).next().html(number);
+                resultStr(counter);
+            }
+        }
+        if (sign == 'plus') {
+            let number = $(e.target).prev().text();
+            if (number < 5) {
+                number++;
+                $(e.target).prev().html(number);
+                resultStr(counter);
+            }
+        }
+    })
+}
+
+for (let btn of document.querySelectorAll('.dropdown__button')) {
+    btn.addEventListener('click', function (e) {
+        this.classList.toggle('dropdown__border');
+        this.nextElementSibling.classList.toggle('show');
+    })
+}
+;
+
+
