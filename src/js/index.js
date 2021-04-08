31b937399d8a7fb2a9fa0ea38edad3529693979c
Nyacist <old.deus.73@gmail.com>
2021-04-08 23:40:31 +0300

    like button, rate botton

--- src/js/index.js
+++ src/js/index.js
@@ -65,6 +65,22 @@ for (let btn of document.querySelectorAll('.dropdown__button')) {
         this.classList.toggle('dropdown__border');
         this.nextElementSibling.classList.toggle('show');
     })
-};
+}
 
+for (let likeCounter of document.querySelectorAll('.like-button__item')) {
+    likeCounter.addEventListener('click', function (e) {
+        let sign = e.target.getAttribute('data-action');
+        let numberOfLikes = e.target.value;
+        if (sign == 'disable') {
+            numberOfLikes++;
+            e.target.setAttribute('data-action', 'enable');
+        }
+        else {
+            numberOfLikes--;
+            e.target.setAttribute('data-action', 'disable');
+        }
+        e.target.parentNode.classList.toggle('like-button__item_enable');
+        e.target.value = numberOfLikes;
+    })
+};
 
