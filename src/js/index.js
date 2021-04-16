a01b9ff3fcbc283145ea6fea5a912a527d3d6e77
Nyacist <old.deus.73@gmail.com>
2021-04-16 17:03:24 +0300

    buttons

--- src/js/index.js
+++ src/js/index.js
@@ -9,54 +9,58 @@ import * as $ from 'jquery'
 import slider from 'jquery-ui/ui/widgets/slider'
 import '../styles/style.scss'
 
+//range slider
+$(function () {
+
+    const space = (num) => String(num).replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
+
+    $(".range-slider__item").slider({
+        range: true,
+        min: 0,
+        max: 15000,
+        values: [5000, 10000],
+        slide: function (event, ui) {
+            $(event.target).prev().children(".range-slider__amount").val(space(ui.values[0]) + "₽" + " - " + space(ui.values[1]) + "₽");
+        }
+    });
+
+    $(".range-slider__amount").val(space($(".range-slider__item").slider("values", 0)) + "₽" +
+        " - " + space($(".range-slider__item").slider("values", 1)) + "₽");
+});
+
 function resultStr(counter) {
     let div = counter.querySelectorAll('.dropdown__count>h3');
-    let str = div[0].textContent +' ';
-    switch (div[0].textContent){
+    let str = div[0].textContent + ' ';
+    switch (div[0].textContent) {
         case '0':
         case '5': {
-            str+= 'спален';
+            str += 'спален';
             break;
         }
         case '1': {
             str += 'спальня';
             break;
         }
-        default: str+= 'спальни';
+        default:
+            str += 'спальни';
     }
-    str+= ', '+div[1].textContent+' ';
-    switch (div[1].textContent){
+    str += ', ' + div[1].textContent + ' ';
+    switch (div[1].textContent) {
         case '0':
         case '5':
-            str+= 'кроватей';
+            str += 'кроватей';
             break;
         case '1':
-            str+= 'кровать';
+            str += 'кровать';
             break;
-        default: str+= 'кровати';
+        default:
+            str += 'кровати';
     }
-    str+= '...';
+    str += '...';
     counter.previousElementSibling.value = str;
 }
 
-$( function() {
-
-    const space = (num) => String(num).replace(/(\d)(?=(\d{3})+(\D|$))/g, '$1 ');
-
-    $( ".range-slider__item" ).slider({
-        range: true,
-        min: 0,
-        max: 15000,
-        values: [ 5000, 10000 ],
-        slide: function( event, ui ) {
-            $(event.target).prev().children(".range-slider__amount").val(space(ui.values[ 0 ]) + "₽" + " - " + space(ui.values[ 1 ]) + "₽" );
-        }
-    });
-
-    $( ".range-slider__amount" ).val(space($( ".range-slider__item" ).slider( "values", 0 )) + "₽" +
-        " - " + space($( ".range-slider__item" ).slider( "values", 1 )) + "₽");
-} );
-
+//counter
 for (let counter of document.querySelectorAll('.dropdown__counter')) {
     counter.addEventListener('click', function (e) {
         let sign = e.target.getAttribute('data-action');
@@ -79,6 +83,7 @@ for (let counter of document.querySelectorAll('.dropdown__counter')) {
     })
 }
 
+//show
 for (let btn of document.querySelectorAll('.dropdown__button')) {
     btn.addEventListener('click', function (e) {
         this.classList.toggle('dropdown__border');
@@ -86,6 +91,7 @@ for (let btn of document.querySelectorAll('.dropdown__button')) {
     })
 }
 
+//like
 for (let likeCounter of document.querySelectorAll('.like-button__item')) {
     likeCounter.addEventListener('click', function (e) {
         let sign = e.target.getAttribute('data-action');
@@ -93,13 +99,22 @@ for (let likeCounter of document.querySelectorAll('.like-button__item')) {
         if (sign == 'disable') {
             numberOfLikes++;
             e.target.setAttribute('data-action', 'enable');
-        }
-        else {
+        } else {
             numberOfLikes--;
             e.target.setAttribute('data-action', 'disable');
         }
         e.target.parentNode.classList.toggle('like-button__item_enable');
         e.target.value = numberOfLikes;
     })
-};
+}
+;
+
+//button disable
+
+for (let btn of document.querySelectorAll('.buttons-block__button')) {
+
+    if (btn.querySelector('.buttons-block__button_background, .buttons-block__button_border').disabled ) {
+        btn.classList.toggle('button-disable');
+    }
+}
 
