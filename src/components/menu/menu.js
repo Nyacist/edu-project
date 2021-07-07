c6e052f3a1f22492e226bc522c30da8db566b984
Nyacist <old.deus.73@gmail.com>
2021-07-07 18:30:15 +0300

    header menu (nav) burger-menu (mobile)

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -4,11 +4,19 @@ import './_menu.scss'
 import * as $ from "jquery";
 
 
-//dropdown
-
-//show
+//menu dropdown show
 for (let btn of document.querySelectorAll('.menu__dropdown-button')) {
     btn.addEventListener('click', function (e) {
         this.nextElementSibling.classList.toggle('show');
+    })
+}
+
+
+for (let btn of document.querySelectorAll('.menu__burger span')) {
+    btn.addEventListener('click', function (e) {
+        let menu = document.querySelector('.menu')
+        menu.classList.toggle('show')
+        menu.classList.toggle('menu__burger-block')
+        menu.classList.toggle('container_horizontal')
     })
 }
