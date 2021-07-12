bf1994f55a7a40cea18dca1341c87bb048031be3
Nyacist <old.deus.73@gmail.com>
2021-07-12 17:17:29 +0300

    footer copyright

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@
 import * as $ from 'jquery'
 import slider from 'jquery-ui/ui/widgets/slider'
 import datepicker from 'jquery-ui/ui/widgets/datepicker'
-import moment from 'moment'
+
 import './pages/formElements/formElements'
 import './pages/cards/cards'
 import './pages/index/headersAndFooters'
--- src/pages/index/headersAndFooters.js
+++ src/pages/index/headersAndFooters.js
@@ -2,3 +2,5 @@ import '../../components/header/header'
 import '../../components/footer/footer'
 
 import './index.scss'
+
+import '@fortawesome/fontawesome-free/js/all'
